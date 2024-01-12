a7daa0bb54dce780d3dad34f40e3dc6c7468ac39
chaeminsu <chaemiinsu@gmail.com>
2024-01-12 15:08:12 +0900

    [Feat][채민수] def load 추가

--- vector_db.py
+++ vector_db.py
@@ -1,4 +1,5 @@
 import os
+import csv
 from openai import OpenAI
 
 openAIclient = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
@@ -10,3 +11,24 @@ def get_embedding(text, model='text-embedding-ada-002'):
     
     vector = response.data[0].embedding
     return vector
+
+
+def load(file_path):
+    vec_db = []
+    with open(file_path, 'r', encoding='utf-8') as f:
+        csv_f = csv.reader(f)
+        next(csv_f) # header row 생락
+
+        for row in csv_f:
+            # id, question, answer
+            text = 'Question: ' + row[1] + "\nAnswer: " + row[2] + '\n'
+            vector = get_embedding(text)
+            doc = {
+                    'id' : row[0],
+                    'vector' : vector,
+                    'question' : row[1] ,
+                    'answer' : row[2]
+            }
+            vec_db.append(doc)
+    
+    return vec_db
