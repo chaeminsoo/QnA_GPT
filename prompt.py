9023ffdb0f0b196400266160c64570106f7c18bf
chaeminsu <chaemiinsu@gmail.com>
2024-01-12 20:18:45 +0900

    [Feat][채민수] prompt.py 추가

---
+++ prompt.py
@@ -0,0 +1,38 @@
+import os
+from openai import OpenAI
+
+from dotenv import load_dotenv
+load_dotenv()
+
+openAIclient = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def make_respense(q, info, temperature=0, max_tokens=1024):
+    prompt = f"""
+    You are a librarian.
+
+    Answer the following question based only on the information given.
+
+    If the given information is not enough to answer the question, just say you do not have enough information, Do not say anything else.
+
+    Question : {q}
+
+    Given information : {info}
+    """
+
+    respense = openAIclient.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role" : "system",
+                "content" : "Create your respense in Korean."
+            },
+            {
+                "role" : "user",
+                "content" : prompt
+            }
+        ],
+        temperature = temperature,
+        max_tokens = max_tokens
+    )
+
+    return respense.choices[0].message.content
